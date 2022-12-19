ad3185559946d28f245370e5bcc13be868b6904c
Akash <adesigns>
2022-12-19 18:12:22 -0500

    added loading page and directories

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import $ from "jquery";
+
+import Loading from "./components/misc-components/Loading";
+
+import Home from "./components/main-components/Home/Home";
+
+import "./styles/root-styles/root.css";
+
+const App = () => {
+    const [loadingStatus, setLoadingStatus] = useState(true);
+
+    const loadingInterval = setInterval(() => {
+        if (document.getElementById("logo-obj").data.indexOf("logo-original") !== -1) {
+            clearInterval(loadingInterval);
+            setTimeout(() => {
+                setLoadingStatus(false);
+                if (!loadingStatus) $("#loading-screen").fadeOut(1000);
+            }, 1500);
+        }
+    }, 500);
+
+    return (
+        <>
+            <Loading />
+            <Home />
+        </>
+    )
+}
+
+export default App;
---
+++ src/components/misc-components/Loading.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+
+import loadingLogo_BLACK from "../../assets/loading-assets/loading-logo.svg";
+import loadingLogo_RED from "../../assets/loading-assets/loading-logo-original-with-text.svg";
+
+import "../../styles/loading-styles/loading.css";
+import $ from "jquery";
+
+const Loading = () => {
+    const getSvgData = (svg) => {
+        let svgDoc = svg.contentDocument;
+        let switchUrl = `http://localhost:3000${loadingLogo_RED}`;
+        let path1 = svgDoc.all[1], path2 = svgDoc.all[2], path3 = svgDoc.all[3];
+
+        $(path1).delay(1000).animate({opacity: 1}, 1000);
+        $(path2).delay(2000).animate({opacity: 1}, 1000);
+        $(path3).delay(3000).animate({opacity: 1}, 1000);
+
+        setTimeout(() => {
+            if (path3.style.opacity === "1") {
+                $(svg).delay(500).animate({opacity: "0"}, 1000);
+                setTimeout(() => {
+                    svg.data = switchUrl;
+
+                    if (svg.data === switchUrl) {
+                        $(svg).delay(1000).animate({opacity: "1"}, 1000);
+                    }
+                }, 2000);
+            }
+        }, 4500);
+    }
+
+    setTimeout(() => {
+        let logoObj = document.getElementById("logo-obj");
+        getSvgData(logoObj);
+    }, 100);
+
+    return (
+        <>
+            <div id = "loading-screen">
+                <div id = "loading-logo">
+                    <object id = "logo-obj" data={loadingLogo_BLACK} type="image/svg+xml" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Loading;
