63c5e4ce6081c1beca3f646951673df6ccf997a9
jnettek <jnettekpelletier@live.com>
2023-01-05 23:36:04 -0500

    Modified & add on

--- src/components/main-components/Header/Header.jsx
+++ src/components/main-components/Header/Header.jsx
@@ -1,12 +1,24 @@
 import "../../../styles/header-styles/header.css";
+import Logo from "../../../assets/main-assets/loading-logo-original.svg"
 import Menu from "./sub-components/Menu";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 
 const Header = () => {
+
     return (
         <>
-           <div id = "header-container">
-                <Menu />
-           </div>
+<div className="header-content">
+        <img src={Logo} alt="WanWan"/>
+        <group>
+            <h1>WAN WAN <span className="italic">CERAMICS</span></h1>
+            <p>A one liner outlining wan wan ceramics</p>
+       </group>
+        <RxHamburgerMenu/>
+    <div id = "header-container">
+    <Menu />
+    </div>
+</div>
         </>
     )
 }
--- src/components/main-components/Header/sub-components/Menu.jsx
+++ src/components/main-components/Header/sub-components/Menu.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
+
 const Menu = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <>
-
         </>
     )
 }
