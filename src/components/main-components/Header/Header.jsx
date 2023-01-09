f55913dc88aa8915f0b2a963646e43d620272d4a
Akash <adesigns>
2023-01-09 13:08:13 -0500

    adjusted header spacings, added href to the header logo

--- src/components/main-components/Header/Header.jsx
+++ src/components/main-components/Header/Header.jsx
@@ -2,6 +2,7 @@ import "../../../styles/header-styles/header.css";
 import Logo from "../../../assets/main-assets/loading-logo-original.svg"
 import { RxHamburgerMenu } from "react-icons/rx";
 import { useGlobalContext } from '../../../utils/context';
+import { links } from "../../../utils/constants";
 
 
 const Header = () => {
@@ -12,11 +13,11 @@ const Header = () => {
         <>
         <div className="header-content">
             <div className="header-content">
-                <img src={Logo} alt="WanWan"/>
-                    <div className="mid-header">
-                        <h1>WAN WAN <span className="italic">CERAMICS</span></h1>
-                            <p>A one liner outlining wan wan ceramics</p>
-                    </div>
+                <a href={links[0].url}><img src={Logo} alt="WanWan" /></a>
+                <div className="mid-header">
+                    <h1>WAN WAN <span className="italic">CERAMICS</span></h1>
+                    <p>A one liner outlining wan wan ceramics</p>
+                </div>
             </div>
                     
         <div className="right-header">
