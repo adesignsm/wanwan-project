7292ea1e7bccc0987136374498596cd91cfac4bc
Akash <adesigns>
2023-02-13 16:40:12 -0500

    recovered project

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,33 @@ import React, { useEffect, useState } from "react";
 
 import $ from "jquery";
 
-// import Loading from "./components/misc-components/Loading";
+import Loading from "./components/misc-components/Loading";
 
 import Header from "./components/main-components/Header/Header";
 import Home from "./components/main-components/Home/Home";
 import Menu from "./components/main-components/Header/sub-components/Menu";
-import Booking from "./components/main-components/Booking/Booking";
+// import Booking from "./components/main-components/Booking/Booking";
 
 import "./styles/root-styles/root.css";
 
 const App = () => {
     const [loadingStatus, setLoadingStatus] = useState(true);
 
-    // const loadingInterval = setInterval(() => {
-    //     if (document.getElementById("logo-obj").data.indexOf("logo-original") !== -1) {
-    //         clearInterval(loadingInterval);
-    //         setTimeout(() => {
-    //             setLoadingStatus(false);
-    //             if (!loadingStatus) $("#loading-screen").fadeOut(1000);
-    //         }, 1500);
-    //     }
-    // }, 500);
+    const loadingInterval = setInterval(() => {
+        if (document.getElementById("logo-obj").data.indexOf("logo-original") !== -1) {
+            clearInterval(loadingInterval);
+            setTimeout(() => {
+                setLoadingStatus(false);
+                if (!loadingStatus) $("#loading-screen").fadeOut(1000);
+            }, 1500);
+        }
+    }, 500);
 
     return (
         <>
-             {/* <Loading /> */}
+            <Loading />
             <Header />
-            {/* <Home /> */}
-            <Booking />
+            <Home />
             <Menu/>
         </>
     )
